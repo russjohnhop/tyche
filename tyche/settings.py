abcfa32550b924a1b74ed2325bf9da86eb2531eb
russjohnhop <47119709+russjohnhop@users.noreply.github.com>
2023-11-26 09:46:34 +0000

    Update settings.py

--- tyche/settings.py
+++ tyche/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-grz_h!rvmu5d4k_9cpx@zw#)9t@yvz-@*@y^8+%1t&1+r707%u'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
